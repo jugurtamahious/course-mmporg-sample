4fe48646156e7605f7f564dd66176f7d51df6d9e
jugurtamahious <jugurtamahious@gmail.com>
2024-11-20 02:44:05 +0100

    fix create palyer while new connection

--- Assets/Demos/MetaVerse/Server/TCPServer.cs
+++ Assets/Demos/MetaVerse/Server/TCPServer.cs
@@ -10,7 +10,7 @@ public class TCPServer : MonoBehaviour
     private TcpListener _tcpListener;
     private List<TcpClient> Clients = new List<TcpClient>();
     public int port = 25000;
-    public string ip = "192.168.0.103";
+    public string ip = "192.168.0.34";
 
 
     public void Start()
@@ -24,11 +24,10 @@ public class TCPServer : MonoBehaviour
         _tcpListener.BeginAcceptTcpClient(OnClientConnect, null);
     }
 
-
     public void Update()
     {
         // Debug.Log("Server updated");
-        ReceiveTCPConnexion();
+        ReceiveTCP();
     }
 
     private void OnApplicationQuit()
@@ -52,40 +51,48 @@ public class TCPServer : MonoBehaviour
         _tcpListener.BeginAcceptTcpClient(OnClientConnect, null);
     }
 
-    private void ReceiveTCPConnexion()
-    {
-        if (_tcpListener == null) { return; }
+    public string ReceiveTCP() {
+        if (_tcpListener == null) { 
+            return null; 
+        }
 
-        while (_tcpListener.Pending())
+         while (_tcpListener.Pending())
         {
             TcpClient tcpClient = _tcpListener.AcceptTcpClient();
-            Debug.Log("New connection received from: " + ((IPEndPoint)tcpClient.Client.RemoteEndPoint).Address);
+            string clientAddress = ((IPEndPoint)tcpClient.Client.RemoteEndPoint).Address.ToString();
+            Debug.Log("New connection received from: " + clientAddress);
             Clients.Add(tcpClient);
+            return $"New connection received from: {clientAddress}";
         }
 
-        foreach (TcpClient client in Clients)
-        {
-            if (!client.Connected)
-            {
+        foreach (TcpClient client in Clients){
+
+            if (!client.Connected) {
                 Debug.Log("Client disconnected");
                 Clients.Remove(client);
-                return;
+                continue; 
             }
 
-            while (client.Available > 0)
-            {
-                byte[] data = new byte[client.Available];
-                client.GetStream().Read(data, 0, client.Available);
+            // while (client.Available > 0) {   
+            //     byte[] data = new byte[client.Available];
+            //     client.GetStream().Read(data, 0, client.Available);
 
-                try
-                {
-                    Debug.Log("Client message: " + System.Text.Encoding.UTF8.GetString(data));
-                }
-                catch (System.Exception ex)
-                {
-                    Debug.LogWarning("Error receiving TCP message: " + ex.Message);
-                }
-            }
+            //     try {
+            //         return ParseString(data); 
+            //     } catch (System.Exception ex) {
+            //         Debug.LogWarning("Error receiving TCP message: " + ex.Message);
+            //         return "false"; 
+            //     }
+            // }
         }
+
+        return null; 
     }
+
+     public string ParseString(byte[] bytes) {
+        string message = System.Text.Encoding.UTF8.GetString(bytes);
+        // OnMessageReceive.Invoke(message);
+        return message;
+    }
+   
 }
